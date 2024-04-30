56f5682602ea9f101546f1c07855a4bf39ee6b4e
Tharadias7 <111554482+Tharadias7@users.noreply.github.com>
2024-04-30 13:54:00 +0530

    registration V5

--- server/routes/Staff.js
+++ server/routes/Staff.js
@@ -9,13 +9,23 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const designation = req.body.designation;
-  const employeeNo = await getNextEmployeeNo(designation);
-  const staff = { ...req.body, employee_no: employeeNo };
-  console.table(staff);
-  await Staff.create(staff);
-  res.json(staff);
+  const { name, contact_no, designation } = req.body;
+
+  try {
+    const employeeNo = await getNextEmployeeNo(designation);
+    const staff = await Staff.create({
+      name,
+      contact_no,
+      designation,
+      employee_no: employeeNo,
+    });
+
+    res.status(201).json(staff);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
+
 
 module.exports = router;
 
